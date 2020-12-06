3988a81403b0f8d37179f5811487249608beb41d
asoftwareworld <asoftwareworld@gmail.com>
2020-12-06 16:54:51 +0530

    add material module

--- projects/grid-demo/src/app/app.module.ts
+++ projects/grid-demo/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],

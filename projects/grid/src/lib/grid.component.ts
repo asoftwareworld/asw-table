c6b158d27c1b8bef9ca2641a632d2298c16ae7a9
asoftwareworld <asoftwareworld@gmail.com>
2020-12-06 12:58:14 +0530

    Added asw-grid project

---
+++ projects/grid/src/lib/grid.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-grid',
+  template: `
+    <p>
+      grid works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class GridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/grid/src/lib/grid.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { GridComponent } from './grid.component';
+
+
+
+@NgModule({
+  declarations: [GridComponent],
+  imports: [
+  ],
+  exports: [GridComponent]
+})
+export class GridModule { }
